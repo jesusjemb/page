31d0c815fa4034173b761ed875af406aff6a810f
Jesus Emmanuel Munguia Barron <jesusemmauelmbjesuse@outlook.es>
2022-12-30 17:26:24 -0600

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,52 @@
 var chain = $("#chains")[0];
 $(".img-wrapper").mouseenter(function() {
-    chain.currentTime = 0;
-		chain.play();
+	chain.currentTime = 0;
+	chain.play();
 });
+
+
+
+document.getElementById("okButton").addEventListener(
+	"click",
+	() => {
+
+		document.body.style.background="#D92323"
+
+		document.getElementById("explicacion").hidden = true;
+		document.getElementById("evento").hidden = false;
+	},
+	false
+	);
+
+
+if (window.localStorage.getItem('AvisoInicial') == undefined
+	) {
+	document.body.style.background="#0D0D0D"
+document.getElementById("explicacion").hidden = false;
+document.getElementById("evento").hidden = true;
+
+}
+
+
+
+window.addEventListener("load", () => {
+  // (A) TEST FETCH HEADER REQUEST TO GOOGLE ADSENSE
+  let test = new Request(
+  	"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+    // "https://static.ads-twitter.com/uwt.js",
+    { method: "HEAD", mode: "no-cors" }
+    );
+
+  // (B) FIRE THE REQEST
+  fetch(test ).then((res) => {
+    //alert("ADS ALLOWED");
+}).catch((err) => {
+
+document.body.style.background="#0D0D0D"
+document.getElementById("explicacion").hidden = false;
+document.getElementById("evento").hidden = true;
+
+});
+});
+
+
